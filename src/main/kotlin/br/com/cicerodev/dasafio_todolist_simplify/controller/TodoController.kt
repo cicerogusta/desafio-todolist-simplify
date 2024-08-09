df75c71c89c69f2f44bbd00c8173f8e3651a09de
cicerodev <ciceroandrade2525@gmail.com>
2024-08-09 19:04:06 -0300

    configurando controller

---
+++ src/main/kotlin/br/com/cicerodev/dasafio_todolist_simplify/controller/TodoController.kt
@@ -0,0 +1,36 @@
+package br.com.cicerodev.desafio_todolist_simplify.controller
+
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import br.com.cicerodev.desafio_todolist_simplify.service.TodoService
+import br.com.cicerodev.dasafio_todolist_simplify.entity.Todo
+
+@RestController
+@RequestMapping("/todos")
+class TodoController(private val todoService:TodoService) {
+
+    fun create(todo: Todo): List<Todo> {
+        todoService.create(todo)
+        return list()
+
+    }
+
+    fun list(): List<Todo> {
+       
+        return todoService.list()
+        
+    }
+
+    fun update(todo:Todo): List<Todo> {
+       todoService.create(todo)
+        return list()
+        
+    }
+
+    fun delete(id: Long): List<Todo> {
+        todoService.delete(id)
+        return list()
+        
+    }
+
+}
