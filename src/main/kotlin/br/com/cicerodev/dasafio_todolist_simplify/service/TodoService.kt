8f85570d420e8c5e4b0da0ff98e29689508e619a
cicerodev <ciceroandrade2525@gmail.com>
2024-08-09 18:53:24 -0300

    configurando service

---
+++ src/main/kotlin/br/com/cicerodev/dasafio_todolist_simplify/service/TodoService.kt
@@ -0,0 +1,35 @@
+package br.com.cicerodev.desafio_todolist_simplify.service
+
+import org.springframework.stereotype.Service
+import org.springframework.data.domain.Sort
+import br.com.cicerodev.dasafio_todolist_simplify.entity.Todo
+import br.com.cicerodev.dasafio_todolist_simplify.repository.TodoRepository
+
+@Service
+class TodoService(private val todoRepository: TodoRepository) {
+
+    fun create(todo: Todo): List<Todo> {
+        todoRepository.save(todo)
+        return list()
+
+    }
+
+    fun list(): List<Todo> {
+       val sort = Sort.by("prioridade").descending().and(Sort.by("nome").ascending()) 
+        return todoRepository.findAll(sort)
+        
+    }
+
+    fun update(todo:Todo): List<Todo> {
+        todoRepository.save(todo)
+        return list()
+        
+    }
+
+    fun delete(id: Long): List<Todo> {
+        todoRepository.deleteById(id)
+        return list()
+        
+    }
+
+}
