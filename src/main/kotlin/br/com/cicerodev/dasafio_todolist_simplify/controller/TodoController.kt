3f529671a92337291be043ce9967aeb0b3c378b7
cicerodev <ciceroandrade2525@gmail.com>
2024-08-09 19:11:39 -0300

    terminando controller

--- src/main/kotlin/br/com/cicerodev/dasafio_todolist_simplify/controller/TodoController.kt
+++ src/main/kotlin/br/com/cicerodev/dasafio_todolist_simplify/controller/TodoController.kt
@@ -2,6 +2,12 @@ package br.com.cicerodev.desafio_todolist_simplify.controller
 
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import br.com.cicerodev.desafio_todolist_simplify.service.TodoService
 import br.com.cicerodev.dasafio_todolist_simplify.entity.Todo
 
@@ -9,25 +15,29 @@ import br.com.cicerodev.dasafio_todolist_simplify.entity.Todo
 @RequestMapping("/todos")
 class TodoController(private val todoService:TodoService) {
 
-    fun create(todo: Todo): List<Todo> {
+    @PostMapping
+    fun create(@RequestBody todo: Todo): List<Todo> {
         todoService.create(todo)
         return list()
 
     }
 
+    @GetMapping
     fun list(): List<Todo> {
        
         return todoService.list()
         
     }
-
-    fun update(todo:Todo): List<Todo> {
+    
+    @PutMapping
+    fun update(@RequestBody todo:Todo): List<Todo> {
        todoService.update(todo)
         return list()
         
     }
 
-    fun delete(id: Long): List<Todo> {
+    @DeleteMapping("{id}")
+    fun delete( @PathVariable("id") id: Long): List<Todo> {
         todoService.delete(id)
         return list()
         
