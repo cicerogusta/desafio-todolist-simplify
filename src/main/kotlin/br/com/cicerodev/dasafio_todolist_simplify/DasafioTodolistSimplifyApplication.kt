77c4a73a3f742cd21406d24b8dc1fe0328c33ad9
cicerodev <ciceroandrade2525@gmail.com>
2024-08-09 17:48:32 -0300

    first commit

---
+++ src/main/kotlin/br/com/cicerodev/dasafio_todolist_simplify/DasafioTodolistSimplifyApplication.kt
@@ -0,0 +1,11 @@
+package br.com.cicerodev.dasafio_todolist_simplify
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DasafioTodolistSimplifyApplication
+
+fun main(args: Array<String>) {
+	runApplication<DasafioTodolistSimplifyApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/cicerodev/dasafio_todolist_simplify/DasafioTodolistSimplifyApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.cicerodev.dasafio_todolist_simplify
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DasafioTodolistSimplifyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
